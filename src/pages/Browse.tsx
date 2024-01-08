e73bace589386b965ff859a1b1a96d90146daed1
Zwyx <29386932+Zwyx@users.noreply.github.com>
2024-01-08 17:18:29 +0800

    Handle invalid characters

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -2,6 +2,14 @@ import { Book, BookPage } from "@/components/BookPage";
 import { Pagination } from "@/components/Pagination";
 import { Privacy } from "@/components/Privacy";
 import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
@@ -11,10 +19,14 @@ import { Message } from "@/lib/worker";
 import { LucideLoader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
+const INVALID_TEXT_REGEX = /[^ abcdefghijklmnopqrstuvwxyz,.]/g;
+
 export const Browse = ({ mode }: { mode: "browse" | "search" }) => {
 	const { worker } = useWorkerContext();
 
 	const [text, setText] = useState<string>("");
+	const [invalidTextDialogOpen, setInvalidTextDialogOpen] =
+		useState<boolean>(false);
 	const [searchOptions, setSearchOptions] = useState<
 		"firstBook" | "pageWithRandom"
 	>("firstBook");
@@ -25,13 +37,32 @@ export const Browse = ({ mode }: { mode: "browse" | "search" }) => {
 
 	const loading = loadingReal || loadingMin;
 
-	const getContent = () => {
+	const getContent = (fixInvalid = false) => {
+		let data = text
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "")
+			.toLowerCase();
+
+		const invalid = data.match(INVALID_TEXT_REGEX);
+
+		if (invalid) {
+			console.log(fixInvalid);
+			if (!fixInvalid) {
+				setInvalidTextDialogOpen(true);
+				return;
+			}
+
+			data = data.replace(INVALID_TEXT_REGEX, "");
+
+			setText(data);
+		}
+
 		setLoadingReal(true);
 		setLoadingMin(true);
 
 		const message: Message = {
 			operation: mode === "search" ? "findBook" : "getBookFromId",
-			data: text,
+			data,
 			options: {
 				find: searchOptions,
 			},
@@ -98,18 +129,55 @@ export const Browse = ({ mode }: { mode: "browse" | "search" }) => {
 				</>
 			)}
 
-			<Button className="my-4" disabled={!text || loading} onClick={getContent}>
+			<Button
+				className="my-4"
+				disabled={!text || loading}
+				onClick={() => getContent()}
+			>
 				<span className={cn(loading && "invisible")}>
 					{mode === "search" ? "Find a book" : "Retrieve the book"}
 				</span>
 
 				<LucideLoader2
 					className={cn(
-						"absolute mr-2 h-4 w-4 animate-spin",
+						"absolute h-4 w-4 animate-spin",
 						!loading && "invisible",
 					)}
 				/>
 			</Button>
+
+			<Dialog
+				open={invalidTextDialogOpen}
+				onOpenChange={setInvalidTextDialogOpen}
+			>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Invalid text</DialogTitle>
+					</DialogHeader>
+
+					<div>
+						The search text contains invalid characters. Would you like them to
+						be automatically removed?
+					</div>
+
+					<div>
+						Valid characters are the space, the letters{" "}
+						<code>abcdefghijklmnopqrstuvwxyz</code>, the comma, and the period.
+					</div>
+
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="secondary">No</Button>
+						</DialogClose>
+
+						<DialogClose asChild>
+							<Button onClick={() => getContent(true)}>
+								Yes, remove invalid characters
+							</Button>
+						</DialogClose>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
 
 			{!book ? (
 				// this is to set the width of the text area
