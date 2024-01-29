97821d5e8fe0b268138345c18c1a02fb4300dac9
Zwyx <29386932+Zwyx@users.noreply.github.com>
2024-01-29 16:35:28 +0800

    Add Plausible and Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -9,6 +10,29 @@ import { WorkerContextProvider } from "./lib/WorkerContext.tsx";
 import { Home } from "./pages/Home.tsx";
 import { Intro } from "./pages/Intro.tsx";
 import { Library } from "./pages/Library.tsx";
+
+const pwa =
+	document.referrer.startsWith("android-app://") ? "twa"
+	: window.matchMedia("(display-mode: standalone)").matches ? "standalone"
+	: "browser";
+
+if (import.meta.env.PROD) {
+	Sentry.init({
+		dsn: import.meta.env.VITE_SENTRY_DSN,
+		environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+		integrations: [
+			new Sentry.BrowserTracing({
+				tracePropagationTargets: [`https://${import.meta.env.VITE_DOMAIN}`],
+			}),
+			Sentry.replayIntegration(),
+		],
+		tracesSampleRate: 0.1,
+		replaysSessionSampleRate: 0.1,
+		replaysOnErrorSampleRate: 1.0,
+	});
+
+	Sentry.setTag("pwa", pwa);
+}
 
 const router = createBrowserRouter([
 	{
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		sentryVitePlugin({
+			org: "zwyx",
+			project: "library-of-babel",
+		}),
+	],
 	resolve: {
 		alias: [
 			{
